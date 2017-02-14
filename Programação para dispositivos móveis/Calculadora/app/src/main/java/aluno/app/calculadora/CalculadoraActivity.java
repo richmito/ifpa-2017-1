f2d51e8c04818b8a038e8e2ce9a92a8ba4a58bf4
ALUNO <ALUNO>
2017-02-14 19:59:24 -0300

    primeiro projeto falho

---
+++ Programação para dispositivos móveis/Calculadora/app/src/main/java/aluno/app/calculadora/CalculadoraActivity.java
@@ -0,0 +1,17 @@
+package aluno.app.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculadoraActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora);
+
+        EditText ednumero1,ednumero2;
+        Button btsomar;
+    }
+}
