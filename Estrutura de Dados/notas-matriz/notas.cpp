389694271fa23da69f6dc08e16208a25055c8d0f
ALUNO <ALUNO>
2017-02-22 17:01:06 -0300

    Funções divididas na forma de biblioteca adicionado arquivo do devcpp convertido para formato UTF-8

---
+++ Estrutura de Dados/notas-matriz/notas.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "notas.h"
+using namespace std;
+/*
+Programa para receber 4 notas das disciplinas de matematica, português, geografia e história.
+Calcula as médias de cada disciplina
+Exibe ao usuário a matriz das notas
+Exibe as médias
+*/
+
+
+int main(int argc, char** argv) {
+	
+	int i,j;
+	receberMatriz(notas);
+	exibirMatriz(notas);
+	cout<<"\nA media dos valores das notas de matematica e igual a "<<medmat(notas);
+	cout<<"\nA media dos valores das notas de portugues e igual a "<<medport(notas);
+	cout<<"\nA media dos valores das notas de geografia e igual a "<<medgeo(notas);
+	cout<<"\nA media dos valores das notas de historia e igual a "<<medhist(notas);
+	return 0;
+}
+
---
+++ Estrutura de Dados/notas-matriz/notas.h
@@ -0,0 +1,130 @@
+﻿// notas.h
+#include <iostream>
+using namespace std;
+const int n = 4;
+typedef float tnota[n][n];
+tnota notas;
+float medmat(tnota &notas)
+{
+	float media;
+	int j,soma=0;
+	for(j=0;j<n;j++){
+		soma = soma+notas[0][j];}
+	media=soma/n;
+	
+	return media;
+		
+}
+
+float medport(tnota &notas)
+{
+	float media;
+	int j,soma=0;
+	for(j=0;j<n;j++){
+		soma = soma+notas[1][j];}
+	media=soma/n;
+	
+	return media;
+		
+}
+
+float medgeo(tnota &notas)
+{
+	float media;
+	int j,soma=0;
+	for(j=0;j<n;j++){
+		soma = soma+notas[2][j];}
+	media=soma/n;
+	
+	return media;
+		
+}
+
+float medhist(tnota &notas)
+{
+	float media;
+	int j,soma=0;
+	for(j=0;j<n;j++){
+		soma = soma+notas[3][j];}
+	media=soma/n;
+	
+	return media;
+		
+}
+
+
+void receberMatriz(tnota notas)
+{
+	//Matriz cuja primeira linha contém as notas de matemática, português, geografia e história, respectivamente.
+	
+	
+	int j;
+	for(j=0;j<n;j++)
+	{
+		cout<<"Insira a "<<j+1<<"a nota de matematica: ";
+		cin>>notas[0][j];
+			
+	}
+	for(j=0;j<n;j++)
+	{
+		cout<<"Insira a "<<j+1<<"a nota de portugues: ";
+		cin>>notas[1][j];
+		
+	}
+	for(j=0;j<n;j++)
+	{
+		cout<<"Insira a "<<j+1<<"a nota de geografia: ";
+		cin>>notas[2][j];
+	}
+	for(j=0;j<n;j++)
+	{
+		cout<<"Insira a "<<j+1<<"a nota de historia: ";
+		cin>>notas[3][j];
+	}
+	system("cls");
+}
+
+void exibirMatriz(tnota notas)
+{
+	int j=0;
+	//Exibir notas de matematica
+	cout<<"Notas de matematica\n";
+	for(j=0;j<n;j++)
+	{
+		//insere tabulação para melhor visualização dos valores das notas
+		cout<<"\t"<<notas[0][j]<<"\t";
+		
+	}
+	cout<<"\n";
+	
+	//Exibir notas de portugues
+	cout<<"Notas de portugues\n";
+	for(j=0;j<n;j++)
+	{
+		cout<<"\t"<<notas[1][j]<<"\t";
+		
+	}
+	cout<<"\n";
+	
+	//Exibir notas de geografia
+	cout<<"Notas de geografia\n";
+	for(j=0;j<n;j++)
+	{
+		cout<<"\t"<<notas[2][j]<<"\t";
+		
+	}
+	cout<<"\n";
+	
+	//Exibir notas de historia
+	cout<<"Notas de historia\n";
+	for(j=0;j<n;j++)
+	{
+		cout<<"\t"<<notas[3][j]<<"\t";
+		
+	}
+	cout<<"\n";
+	
+	
+	
+}
+
