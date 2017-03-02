71624d882b6c9f7355a4012414afe47edeb0d208
Richelmy Monteiro <richmito@users.noreply.github.com>
2017-03-02 16:46:14 -0300

    Exercício de Pesquisa binária inicial
    não deu tempo de terminar em aula :(
    Tutorial de como fazer no SIGAA, será uploadeado quando o exercício for terminado

---
+++ Estrutura de Dados/Pesquisa Binaria/PesquisaBinaria.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+/**
+receba 10 valores em um vetor, use busca binária para encontrar determinado valor nesse vetor
+*/
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+const n=10;
+typedef float tdado;
+tdado vetor[n],vetorOrdenado[n];
+void ReceberVetor()
+{
+	int i=0;
+	for(i=0;i<n;i++)
+	{
+		//cout<<"\nInsira o valor do vetor da posicao"<<i+1<<": ";
+		//cin<<vetor[i];
+		vetor[i]=i;
+	}
+}
+
+void OrdenarVetor(tdado vetor)
+{
+		
+}
+
+int BuscaBinaria(tdado vetorOrdenado)
+{
+	int menor = 0,
+		maior = n-1 // posição máxima do vetor, menos um, devido a inteiros começarem com 0
+		meio = (baixo+alto)/2; // calcula a média do tamanho do vetor para definir seu meio
+		
+	
+	
+	for(i=0;i<n;i++)
+		if(vetor[i]==)
+			{
+				
+			}
+				
+	
+}
+
+
+int main() {
+	
+	
+	
+	return 0;
+}
