266bcd8825cafaefbfad20f058f16dccf5af962e
Richelmy Monteiro <richmito@users.noreply.github.com>
2017-03-08 17:00:51 -0300

    Segunda tentativa de pesquisa binária
    sim, eu sei que ele não quebra o vetor mais que uma vez, corrigir isso

--- Estrutura de Dados/Pesquisa Binaria/PesquisaBinaria.cpp
+++ Estrutura de Dados/Pesquisa Binaria/PesquisaBinaria.cpp
@@ -1,50 +1,78 @@
 ﻿#include <iostream>
-/**
-receba 10 valores em um vetor, use busca binária para encontrar determinado valor nesse vetor
-*/
+using namespace std;
+/** pesquisa binária
+recebe 10 valores em um vetor
+coloca os valores em ordem crescente
+verifica 
+
 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-const n=10;
-typedef float tdado;
-tdado vetor[n],vetorOrdenado[n];
-void ReceberVetor()
+const int tamanho=10;
+typedef float tvetor[tamanho];
+tvetor vetor,vetorOrdenado;
+
+
+
+void receberVetor(tvetor vetor)
 {
 	int i=0;
-	for(i=0;i<n;i++)
+	for(i=0;i<tamanho;i++)	
 	{
-		//cout<<"\nInsira o valor do vetor da posicao"<<i+1<<": ";
-		//cin<<vetor[i];
-		vetor[i]=i;
+		cout<<"Insira o valor da posicao no "<<i<<" : ";
+		cin>>vetor[i];
 	}
+	system("cls");
 }
 
-void OrdenarVetor(tdado vetor)
+
+void ordemCrescente()
 {
+	
+}
+
+void pesquisaBinaria(tvetor vetor, int tamanho,float busca)
+{
+	/*
+	verifica o elemento medio inicial do vetor
+	comparar valor a ser buscado pelo elemento médio
+	se valor buscado for igual, retornar
+	se for maior, definir contador como media+1 e contar para mais
+	se for menor, definir contador como media-1 e contar pra menos
+	*/
+	
+	int media,
+	maior=vetor[tamanho], // vetor ordenado possui maior valor em sua última posição, correspondente ao seu tamanho total
+	menor=0; //menor sempre zero, pra ser preenchido na primeira leva da busca
+	media = maior/2; // Valor inicial da media 
+	if(media==busca)
+		return busca; // se valor buscado for igual a media, retornar media
+	else if(busca>media) // se valor buscado for maior que a media, define a busca pra parte superior do vetor
+		for(i=media+1;i<maior;i++)
+		{
+			if(busca==vetor[i])
+				return vetor[i];
+		}
+	else if(busca<media)
+		for(i=media-1;i>menor;i--)
+		{
+			if(busca==vetor[i])
+				return vetor[i];
+		}
+		
+	
+void receberVetor()
+{
+	
 		
 }
 
-int BuscaBinaria(tdado vetorOrdenado)
-{
-	int menor = 0,
-		maior = n-1 // posição máxima do vetor, menos um, devido a inteiros começarem com 0
-		meio = (baixo+alto)/2; // calcula a média do tamanho do vetor para definir seu meio
-		
 	
-	
-	for(i=0;i<n;i++)
-		if(vetor[i]==)
-			{
-				
-			}
-				
 	
 }
 
 
-int main() {
-	
-	
-	
+
+int main(int argc, char** argv) {
 	return 0;
 }
