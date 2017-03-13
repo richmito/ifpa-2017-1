dfae04e44403fcb5c4a94ceff19084bd90fe4ac6
Richelmy Monteiro <richmito@users.noreply.github.com>
2017-03-13 17:55:22 -0300

    Função de pesquisa funcionando, ordenação pendente

--- Estrutura de Dados/Pesquisa Binaria/PesquisaBinaria.cpp
+++ Estrutura de Dados/Pesquisa Binaria/PesquisaBinaria.cpp
@@ -4,25 +4,29 @@ using namespace std;
 recebe 10 valores em um vetor
 coloca os valores em ordem crescente
 verifica 
-
-
-
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 const int tamanho=10;
-typedef float tvetor[tamanho];
+typedef int tvetor[tamanho];
 tvetor vetor,vetorOrdenado;
+int maior=tamanho,menor=0,meio;
+int busca=0;
 
 
-
-void receberVetor(tvetor vetor)
+void receberVetor(tvetor &vetor)
 {
+	
 	int i=0;
 	for(i=0;i<tamanho;i++)	
 	{
-		cout<<"Insira o valor da posicao no "<<i<<" : ";
+		cout<<"Insira o valor da posicao no "<<i+1<<" : ";
 		cin>>vetor[i];
 	}
-	system("cls");
+	//system("cls"); 
+
+
+
+
+
 }
 
 
@@ -31,48 +35,45 @@ void ordemCrescente()
 	
 }
 
-void pesquisaBinaria(tvetor vetor, int tamanho,float busca)
+int pesquisaBinaria(tvetor &vetor, int tamanho,int busca)
 {
 	/*
-	verifica o elemento medio inicial do vetor
-	comparar valor a ser buscado pelo elemento médio
-	se valor buscado for igual, retornar
-	se for maior, definir contador como media+1 e contar para mais
-	se for menor, definir contador como media-1 e contar pra menos
+	Enquanto valor menor for menor que valor maior
+	valor do meio é igual a valor
+	verifique se o valor do meio é igual ao valor buscado
+	se for menor, valor superior se torna a média anterior
+	se for maior, valor inferior se torna a média anterior
 	*/
+
+	while(menor<=maior)
+	{
+	//cout<<"Laco iniciou";
+	meio=(maior+menor)/2;
 	
-	int media,
-	maior=vetor[tamanho], // vetor ordenado possui maior valor em sua última posição, correspondente ao seu tamanho total
-	menor=0; //menor sempre zero, pra ser preenchido na primeira leva da busca
-	media = maior/2; // Valor inicial da media 
-	if(media==busca)
-		return busca; // se valor buscado for igual a media, retornar media
-	else if(busca>media) // se valor buscado for maior que a media, define a busca pra parte superior do vetor
-		for(i=media+1;i<maior;i++)
-		{
-			if(busca==vetor[i])
-				return vetor[i];
-		}
-	else if(busca<media)
-		for(i=media-1;i>menor;i--)
-		{
-			if(busca==vetor[i])
-				return vetor[i];
-		}
+	if(busca==vetor[meio]){
 		
+		return meio+1;} // função retorna o índice do vetor que contém o valor desejado
+	else if(busca<meio){
+		maior=meio-1; } // se a busca for menor que o valor do meio, a busca continua do meio pra baixo
+		else
+		{menor=meio+1;}// se a busca for maior que o valor do meio, busca continua do valor do meio pra cima
 	
-void receberVetor()
-{
+	
 	
 		
+		
+	}
 }
-
+		
 	
+int main() {
+	receberVetor(vetor);
 	
-}
-
-
-
-int main(int argc, char** argv) {
+	cout<<"Insira o valor a ser buscado ";
+	cin>>busca;
+	
+	cout<<"O indice do vetor que contem o valor e o "<<pesquisaBinaria(vetor,tamanho,busca);
+	
 	return 0;
 }
+
