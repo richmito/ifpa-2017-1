31a2546fd2eea9712a358caf90ca152bffc634b4
Richelmy Monteiro <richmito@users.noreply.github.com>
2017-03-18 00:21:01 -0300

    nome de variável corrigido

--- Sistemas Distribuídos/lerURL/src/lerURL/lerURL.java
+++ Sistemas Distribuídos/lerURL/src/lerURL/lerURL.java
@@ -6,9 +6,9 @@ import java.io.*;
 public class lerURL {
 public static void main(String[] args) throws IOException, Exception{
 		
-	URL oracle = new URL("https://raw.githubusercontent.com/richmito/ifpa-2017-1/master/helloworld.txt");
+	URL endereco = new URL("https://raw.githubusercontent.com/richmito/ifpa-2017-1/master/helloworld.txt");
     BufferedReader in = new BufferedReader(
-    new InputStreamReader(oracle.openStream()));
+    new InputStreamReader(endereco.openStream()));
 
     String inputLine;
     while ((inputLine = in.readLine()) != null)
