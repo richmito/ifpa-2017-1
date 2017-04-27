2922c39e4028b51082abb297ce55258cd433ad4e
richmito <richelmypalmito@hotmail.com>
2017-04-27 17:10:01 -0300

    Exercício de pilha baseada em vetor, 1 de 3 funções feitas

---
+++ Estrutura de Dados/Estruturas de dados/Pilha/vetor.h
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+// declaração das variáveis
+const int n=5;
+typedef float tDado[n];
+bool OK = true;
+
+struct tPilha(
+  int topper;
+  tDado elemento[n];
+
+)
+
+void empilhar(float elemento){
+
+  if(pilha.topper==n){// verifica se a pilha já atingiu seu tamanho máximo
+  OK=false;
+}else{
+  pilha.elemento[pilha.topper]=elemento; // pilha recebe elemento passado por parâmetro
+  pilha.topper++; // topo da pilha é incrementado
+
+}
+
+
+}
+
+void desempilhar(){
+  if()
+
+
+}
+
+void acessarTopo(){
+
+}
+
+ void helloworld(){
+   cout<<"\nHello World\n";
+ }
